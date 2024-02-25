d14a7b7729d8ccc10c2228fbde38de8d73f27e5f
calalty <callum.alton33@gmail.com>
2024-02-25 23:22:14 +0000

    test

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -11,8 +11,34 @@ type Props = {
   initialMessages?: Message[];
 };
 
-export const MessageList = async ({ initialMessages }: Props) => {
-  const messages = await fetcher();
+export const MessageList = ({ initialMessages }: Props) => {
+  const { data: messages, mutate } = useSWR<Message[]>(
+    "/api/getMessages",
+    fetcher,
+    { revalidateOnFocus: true }
+  );
+
+  useEffect(() => {
+    const channel = clientPusher.subscribe("messages");
+
+    channel.bind("new-message", async (data: Message) => {
+      if (messages?.find((message) => message.id === data.id)) return;
+
+      if (!messages) {
+        mutate(fetcher);
+      } else {
+        mutate(fetcher, {
+          optimisticData: [data, ...messages!],
+          rollbackOnError: true,
+        });
+      }
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages, mutate]);
 
   return (
     <ul className="space-y-5 px-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
