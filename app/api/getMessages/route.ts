97c6c567151bc71bc0c7866952b70b8318611504
calalty <callum.alton33@gmail.com>
2024-02-25 23:53:55 +0000

    change

--- app/api/getMessages/route.ts
+++ app/api/getMessages/route.ts
@@ -2,6 +2,8 @@ import redis from "../../../redis";
 import { Message } from "@/typings";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const messagesRes = await redis.hvals("messages");
 
