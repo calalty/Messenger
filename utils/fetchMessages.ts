ebbb6f8cd0fbc47d38df75d246937e5e699768f6
calalty <callum.alton33@gmail.com>
2024-02-25 23:20:28 +0000

    test

--- utils/fetchMessages.ts
+++ utils/fetchMessages.ts
@@ -1,17 +1,9 @@
 import { Message } from "@/typings";
 
 export const fetcher = async () => {
-  try {
-    const timestamp = new Date().getTime();
-    const res = await fetch(`/api/getMessages?timestamp=${timestamp}`);
-    console.log("Response Status:", res.status);
-    console.log("Response Headers:", res.headers);
-    const data = await res.json();
-    const messages: Message[] = data;
+  const res = await fetch("/api/getMessages");
+  const data = await res.json();
+  const messages: Message[] = data;
 
-    return messages;
-  } catch (error) {
-    console.error("Fetch Error:", error);
-    throw error;
-  }
+  return messages;
 };
