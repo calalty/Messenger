101ac3abd239b6f7da7c9a253aecbe8fa89a60f0
calalty <callum.alton33@gmail.com>
2024-02-25 22:13:01 +0000

    test

--- utils/fetchMessages.ts
+++ utils/fetchMessages.ts
@@ -5,6 +5,6 @@ export const fetcher = async () => {
   const data = await res.json();
   const messages: Message[] = data;
 
-  console.log('FETCHER'{messages})
+  console.log('FETCHER',{messages})
   return messages;
 };
