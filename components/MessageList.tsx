311100f8188240cc738150dd22d06bc30f3874a7
calalty <callum.alton33@gmail.com>
2024-02-26 22:20:22 +0000

    changed env name as its reserved

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -41,7 +41,7 @@ export const MessageList = ({ initialMessages }: Props) => {
 
   return (
     <ul className="space-y-5 px-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
-      {(messages || initialMessages)?.map((message) => (
+      {(messages ?? initialMessages)?.map((message) => (
         <li key={message.id}>
           <MessageBubble message={message} />
         </li>
