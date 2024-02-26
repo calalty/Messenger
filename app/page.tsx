9e1d36396ee268e817986f9dba2a0378dcc86706
calalty <callum.alton33@gmail.com>
2024-02-26 21:45:05 +0000

    add

--- app/page.tsx
+++ app/page.tsx
@@ -7,42 +7,24 @@ import { getServerSession } from "next-auth";
 import { Providers } from "./providers";
 import { Message } from "@/typings";
 
-export const dynamic = "force-dynamic";
-
 const Home = async () => {
-  try {
-    const session = await getServerSession();
+  const session = await getServerSession();
 
-    // Log the session to check if it's obtained successfully
-    console.log("Session:", session);
+  const messages: Message[] = await fetch(
+    `${process.env.VERCEL_URL}api/getMessages`,
+    { cache: "no-store" }
+  ).then((res) => res.json());
 
-    const apiUrl = `${process.env.VERCEL_URL}api/getMessages`;
-
-    // Log the API URL to check its correctness
-    console.log("API URL:", apiUrl);
-
-    const response = await fetch(apiUrl, { cache: "no-store" });
-
-    // Log the raw response for inspection
-    console.log("Raw Response:", response);
-
-    const messages: Message[] = await response.json();
-
-    // Log the messages to check if they are retrieved successfully
-    console.log("Messages:", messages);
-
-    return (
-      <main>
-        <Providers session={session}>
-          <MessageList initialMessages={messages} />
+  return (
+    <Providers session={session}>
+      {session && (
+        <main>
+          {messages && <MessageList initialMessages={messages} />}
           <ChatInput session={session} />
-        </Providers>
-      </main>
-    );
-  } catch (error) {
-    console.error("Error in Home component:", error);
-    throw error;
-  }
+        </main>
+      )}
+    </Providers>
+  );
 };
 
 export default Home;
